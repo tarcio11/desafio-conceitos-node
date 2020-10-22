e4478772ec31fc4fd33c36ed71b03b456fc2df77
Tarcio <tarcio_x@hotmail.com>
2020-10-22 16:52:08 -0300

    Challenge completed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4');
 
 // const { v4: uuid } = require('uuid');
 
@@ -10,24 +11,91 @@ app.use(cors());
 
 const repositories = [];
 
+/**
+ * Adicionando Middleware nas rotas com ID
+ */
+
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Inválid Repository ID." })
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId) // usando em todas as rotas com id
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (findRepositoryIndex >= 0) {
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[findRepositoryIndex].likes,
+    }
+
+    repositories[findRepositoryIndex] = repository;
+
+    return response.json(repository);
+  } else {
+    return response.status(400).json({ error: "Inválid Repository ID." })
+  }
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (findRepositoryIndex >= 0) {
+    repositories.splice(findRepositoryIndex, 1);
+
+    return response.status(204).send()
+  } else {
+    return response.status(400).json({ error: "Inválid Repository ID." });
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (findRepositoryIndex === -1) {
+    return response.status(400).json({ error: "Inválid Repository ID." });
+  }
+
+  repositories[findRepositoryIndex].likes += 1;
+
+  return response.json(repositories[findRepositoryIndex])
 });
 
 module.exports = app;
